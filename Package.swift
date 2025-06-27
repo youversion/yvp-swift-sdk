e5db49a1119cb6cc9423a8a92a7a7aaeea1d86d3
Bryan Montz <bryan.montz@youversion.com>
2025-06-27 11:01:01 -0500

    try macOS 14

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "YouVersionPlatform",
-    platforms: [.macOS(.v15), .iOS(.v17)],
+    platforms: [.macOS(.v14), .iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(

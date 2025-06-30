fd6243e1c8c3ace278424a0b111ae25d116f7c8d
Bryan Montz <bryanmontz@me.com>
2025-06-30 10:18:53 -0500

    update VotdView to use the full model object from network (#19)

--- Sources/Views/VotdView.swift
+++ Sources/Views/VotdView.swift
@@ -1,45 +1,56 @@
 import SwiftUI
 
 public struct VotdView: View {
-    public let votdTitle: String
-    public let votdText: String
-    public let votdCopyright: String
+    @State private var votd: YouVersionVerseOfTheDay?
+    
     public let backgroundUrl: String?
     public let minHeight: CGFloat?
     public let maxHeight: CGFloat?
-    public let dayNumber: Int? = 112
     var lineLimit: Int?  // do we want this?
     
     public init(
-        votdTitle: String,
-        votdText: String,
-        votdCopyright: String,
+        votd: YouVersionVerseOfTheDay? = nil,
         backgroundUrl: String? = nil,
         minHeight: CGFloat? = nil,
         maxHeight: CGFloat? = nil
     ) {
-        self.votdTitle = votdTitle
-        self.votdText = votdText
-        self.votdCopyright = votdCopyright
+        self._votd = State(initialValue: votd)
         self.backgroundUrl = backgroundUrl
         self.minHeight = minHeight
         self.maxHeight = maxHeight
     }
     
     public var body: some View {
-        basicText
-            .padding()
-            .foregroundColor(.white)
-            .frame(minHeight: minHeight, maxHeight: maxHeight)
-            .background(
-                GeometryReader { geo in
-                    if let backgroundUrl, let url = URL(string: backgroundUrl) {
-                        votdBackground(url: url)
-                            .frame(width: geo.size.width, height: geo.size.height)
-                            .clipped()
-                    }
-                }
-            )
+        Group {
+            if let votd {
+                text(votd: votd)
+                    .padding()
+                    .frame(minHeight: minHeight, maxHeight: maxHeight)
+                    .background(
+                        GeometryReader { geo in
+                            if let backgroundUrl, let url = URL(string: backgroundUrl) {
+                                votdBackground(url: url)
+                                    .frame(width: geo.size.width, height: geo.size.height)
+                                    .clipped()
+                            }
+                        }
+                    )
+            } else {
+                ProgressView()
+                    .scaleEffect(2)
+            }
+        }
+        .task {
+            guard votd == nil else {
+                return
+            }
+            
+            do {
+                votd = try await verseOfTheDay(versionId: 111)
+            } catch {
+                print("VotdView: error loading votd: \(error)")
+            }
+        }
     }
     
     private func votdBackground(url: URL) -> some View {
@@ -55,20 +66,20 @@ public struct VotdView: View {
         }
     }
     
-    var basicText: some View {
+    private func text(votd: YouVersionVerseOfTheDay) -> some View {
         VStack(alignment: .leading) {
-            Text(votdTitle)
+            Text("\(votd.reference) (\(votd.abbreviation ?? ""))")
                 .font(.title.bold())
                 .lineLimit(1)
                 .padding(.bottom, 16)
-            Text(votdText)
+            Text(votd.text ?? "")
                 .multilineTextAlignment(.leading)
                 .font(.system(size: 24, design: .serif))
                 .minimumScaleFactor(0.5)
                 .lineLimit(lineLimit)
                 .textSelection(.enabled)
                 .padding(.bottom, 16)
-            Text(votdCopyright)
+            Text(votd.copyright ?? "")
                 .font(.caption)
                 .minimumScaleFactor(0.1)
         }
@@ -79,11 +90,12 @@ public struct VotdView: View {
     VStack {
         Spacer()
         VotdView(
-            votdTitle: "John 3:16 (NIV)",
-            votdText: "For God so loved the world that he gave his one and only Son, that whoever believes in " +
-                      "him shall not perish but have eternal life.",
-            votdCopyright: "The Holy Bible, New International Version® NIV®\nCopyright © 1973, 1978, 1984, 2011 " +
-                           "by Biblica, Inc.®\nUsed by Permission of Biblica, Inc.® All rights reserved worldwide.",
+            votd: YouVersionVerseOfTheDay(
+                reference: "John 3:16",
+                abbreviation: "NIV",
+                text: "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.",
+                copyright: "The Holy Bible, New International Version® NIV®\nCopyright © 1973, 1978, 1984, 2011 by Biblica, Inc.®\nUsed by Permission of Biblica, Inc.® All rights reserved worldwide."
+            ),
             backgroundUrl: "https://imageproxy.youversionapi.com/1500x/" +
                            "https://votd-background-images-prod.storage.googleapis.com/113.jpg",
             minHeight: 200,
